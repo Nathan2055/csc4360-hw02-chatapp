3f2b186254c1f6bce1d8112206db4d35b1dd5652
Nathan Larsen <nlarsen@nathan2055.com>
2025-11-18 21:37:51 -0500

    Refactor and comment AuthService

--- lib/authservice.dart
+++ lib/authservice.dart
@@ -1,13 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:chatapp/models/firestore_helper.dart';
 import 'package:chatapp/models/user_entry.dart';
 
+// AuthService handles all functions associated with user authentication
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirestoreHelper userDatabase;
 
   AuthService(this.userDatabase);
 
+  // Creates an account and an associated profile and then logs in
   void createAccount(
     String emailAddress,
     String password,
@@ -32,29 +35,33 @@ class AuthService {
     } on FirebaseAuthException catch (e) {
       // TODO: pass exceptions up to a snackbar
       if (e.code == 'weak-password') {
-        print('The password provided is too weak.');
+        debugPrint('The password provided is too weak.');
       } else if (e.code == 'email-already-in-use') {
-        print('The account already exists for that email.');
+        debugPrint('The account already exists for that email.');
       }
     } catch (e) {
-      print(e);
+      debugPrint(e.toString());
     }
   }
 
+  // Log in to the app with an email address and password
   void login(String emailAddress, String password) {
     try {
       _auth.signInWithEmailAndPassword(email: emailAddress, password: password);
     } on FirebaseAuthException catch (e) {
+      // TODO: pass exceptions up to a snackbar
       if (e.code == 'user-not-found') {
-        print('No user found for that email.');
+        debugPrint('No user found for that email.');
       } else if (e.code == 'wrong-password') {
-        print('Wrong password provided for that user.');
+        debugPrint('Wrong password provided for that user.');
       }
     } catch (e) {
-      print(e);
+      debugPrint(e.toString());
     }
   }
 
+  // Updates the password for the currently logged in user
+  // Returns a Future that resolves to true if successful and false on failure
   Future<bool> updatePassword(String oldPassword, String newPassword) async {
     try {
       // Get the info for the current user
@@ -74,13 +81,16 @@ class AuthService {
 
       return true;
     } catch (e) {
-      print(e);
+      debugPrint(e.toString());
       return false;
     }
   }
 
-  // Firebase does not allow updating emails without handling
-  // email verification, which is beyond the scope of this homework
+  // This would be where I implemented a mechanism to update email
+  // addresses. However, Google has updated the Firebase library without
+  // updating their documentaton, and apparently no longer allows updating
+  // email addresses without also handling verification emails at the same time,
+  // which is beyond the scope of this homework.
   /*
   void updateEmail(String newEmail, String password) async {
     try {
@@ -105,31 +115,22 @@ class AuthService {
   }
   */
 
+  // Log out of the app
   void logout() {
-    print('signing out');
     try {
       _auth.signOut();
-      print('signed out');
-    } on FirebaseAuthException catch (e) {
-      print(e);
+    } catch (e) {
+      debugPrint(e.toString());
     }
   }
 
-  bool isLoggedIn() {
-    if (_auth.currentUser != null) {
-      print('current user:');
-      print(_auth.currentUser?.uid);
-      return true;
-    } else {
-      print('not signed in');
-      return false;
-    }
-  }
-
+  // Get a Stream of the app's authentication state
   Stream<User?>? getStream() {
     return _auth.authStateChanges();
   }
 
+  // Get the currently logged in user's email address
+  // Returns an empty string if the user is not logged in
   String getEmail() {
     if (_auth.currentUser != null) {
       for (final providerProfile in _auth.currentUser!.providerData) {
