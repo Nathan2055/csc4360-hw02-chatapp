828df315ab321cdf9b39655f476235112fcb303e
Nathan Larsen <nlarsen@nathan2055.com>
2025-11-18 21:51:49 -0500

    Start adding chat functions to FirestoreHelper

--- lib/models/firestore_helper.dart
+++ lib/models/firestore_helper.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:chatapp/models/user_entry.dart';
+import 'package:chatapp/models/chat_entry.dart';
 
 // FirestoreHelper handles all interaction with the Cloud Firestore databases
 class FirestoreHelper {
@@ -118,6 +119,107 @@ class FirestoreHelper {
       _firestore.collection('users').doc(id).delete();
     } catch (e) {
       debugPrint('Error deleting item: $e');
+    }
+  }
+  */
+
+  // Chat functions start here
+
+  /*
+  void addChatEntry(String collection, ChatEntry chatEntry) {
+    try {
+      _firestore.collection(collection).add(chatEntry.toMap());
+    } catch (e) {
+      debugPrint('Error creating item: $e');
+    }
+  }
+
+  Stream<List<ChatEntry>> getChatEntryStream(String collection) {
+    return _firestore
+        .collection(collection)
+        .snapshots()
+        .map(
+          (snapshot) =>
+              snapshot.docs.map((doc) => ChatEntry.fromSnapshot(doc)).toList(),
+        );
+  }
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> getRawChatEntryStream(
+    String collection,
+  ) {
+    return _firestore.collection(collection).snapshots();
+  }
+
+  void updateChatEntry(String collection, ChatEntry chatEntry) {
+    try {
+      _firestore
+          .collection(collection)
+          .doc(chatEntry.id)
+          .update(chatEntry.toMap());
+      debugPrint('ChatEntry updated');
+    } catch (e) {
+      debugPrint('Error updating item: $e');
+    }
+  }
+
+  void updateChatEntryByID(
+    String collection,
+    String id,
+    String name,
+    double price,
+  ) {
+    try {
+      _firestore.collection(collection).doc(id).update({
+        "name": name,
+        "price": price,
+      });
+    } catch (e) {
+      debugPrint('Error updating item: $e');
+    }
+  }
+
+  void deleteChatEntry(String collection, String id) {
+    try {
+      _firestore.collection(collection).doc(id).delete();
+    } catch (e) {
+      debugPrint('Error deleting item: $e');
+    }
+  }
+
+  // Firestore query for search
+  Stream<QuerySnapshot<Object?>>? searchProducts(
+    String collection,
+    String query,
+  ) {
+    try {
+      var result = _firestore
+          .collection(collection)
+          .where('name', isGreaterThanOrEqualTo: query)
+          .where('name', isLessThanOrEqualTo: '$query\uf8ff')
+          .snapshots();
+      return result;
+    } catch (e) {
+      debugPrint('Error searching items: $e');
+      return null;
+    }
+  }
+
+  // Firestore query for price filter
+  Stream<QuerySnapshot<Object?>>? filterProductsByPrice(
+    String collection,
+    double minPrice,
+    double maxPrice,
+  ) {
+    try {
+      var result = _firestore
+          .collection(collection)
+          .where('price', isGreaterThanOrEqualTo: minPrice)
+          .where('price', isLessThanOrEqualTo: maxPrice)
+          .snapshots();
+      return result;
+    } catch (e) {
+      debugPrint('Error filtering items: $e');
+      return null;
     }
   }
   */
