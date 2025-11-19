b0c248a14770b979047455fd5d86f1649331672a
Nathan Larsen <nlarsen@nathan2055.com>
2025-11-18 22:49:24 -0500

    Finish work on MessageBoard class

--- lib/screens/3-message_board/message_board.dart
+++ lib/screens/3-message_board/message_board.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:chatapp/authservice.dart';
-import 'package:chatapp/models/user_entry.dart';
 import 'package:chatapp/models/firestore_helper.dart';
-import 'package:chatapp/screens/4-profile_screen/profile_screen.dart';
-import 'package:chatapp/screens/5-settings_screen/settings_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:chatapp/models/user_entry.dart';
 import 'package:chatapp/models/chat_entry.dart';
 
 class MessageBoard extends StatefulWidget {
@@ -25,16 +21,10 @@ class MessageBoard extends StatefulWidget {
 }
 
 class _MessageBoardState extends State<MessageBoard> {
-  late AppBar homeScreenAppBar;
-
-  late Column _homeColumn;
-
   late String _email;
 
   late Stream<QuerySnapshot> _chatStream;
-  late StreamBuilder<QuerySnapshot> _chatStreamWidget;
 
-  late Form _chatForm;
   final TextEditingController _messageController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
 
@@ -47,85 +37,7 @@ class _MessageBoardState extends State<MessageBoard> {
     super.initState();
 
     _chatStream = widget.dbHelper.getChatStream(widget.messageBoard);
-
     _email = widget.authService.getEmail();
-
-    _buildChatStreamWidget();
-    _buildChatForm();
-  }
-
-  void _buildChatStreamWidget() {
-    _chatStreamWidget = StreamBuilder<QuerySnapshot>(
-      stream: _chatStream,
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return Center(
-            child: SingleChildScrollView(
-              padding: const EdgeInsets.all(64.0),
-              child: Column(
-                children: [
-                  const Text(
-                    'Something went wrong',
-                    style: TextStyle(
-                      fontSize: 22.0,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        }
-
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(
-            child: SingleChildScrollView(
-              padding: const EdgeInsets.all(64.0),
-              child: Column(
-                children: [
-                  const Text(
-                    'Loading',
-                    style: TextStyle(
-                      fontSize: 22.0,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        }
-
-        return ListView(
-          children: snapshot.data!.docs
-              .map((DocumentSnapshot document) {
-                Map<String, dynamic> data =
-                    document.data()! as Map<String, dynamic>;
-                ChatEntry message = ChatEntry.fromMap(data);
-
-                // Null/emptiness check
-                if (message.message == null ||
-                    message.userEmail == null ||
-                    message.createdAt == null ||
-                    message.message == '' ||
-                    message.userEmail == '') {
-                  return Container();
-                }
-
-                String username = _getUsernameFromEmail(message.userEmail!);
-
-                return ListTile(
-                  title: Text(message.message!),
-                  subtitle: Text(
-                    'Sent by $username at ${message.createdAt!.toString()}',
-                  ),
-                );
-              })
-              .toList()
-              .cast(),
-        );
-      },
-    );
   }
 
   String _getUsernameFromEmail(String email) {
@@ -148,40 +60,6 @@ class _MessageBoardState extends State<MessageBoard> {
     while (!complete) {}
 
     return username;
-  }
-
-  void _buildChatForm() {
-    _chatForm = Form(
-      key: _formKey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        spacing: 24.0,
-        children: [
-          // Username field
-          TextFormField(
-            enabled: (_sendingMessage == 'ready'),
-            controller: _messageController,
-            decoration: InputDecoration(
-              labelText: 'Chat Message',
-              prefixIcon: const Icon(Icons.message),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-            ),
-          ),
-
-          // Submit button
-          ElevatedButton(
-            onPressed: (_sendingMessage != 'ready') ? null : _submitChatForm,
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [Text('Send Message')],
-            ),
-          ),
-        ],
-      ),
-    );
   }
 
   void _submitChatForm() {
@@ -223,12 +101,127 @@ class _MessageBoardState extends State<MessageBoard> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: homeScreenAppBar,
-      body: Center(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(64.0),
-          child: _homeColumn,
+    return Center(
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.all(64.0),
+        child: Column(
+          children: [
+            // Chat message stream
+            StreamBuilder<QuerySnapshot>(
+              stream: _chatStream,
+              builder:
+                  (
+                    BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot,
+                  ) {
+                    if (snapshot.hasError) {
+                      return Center(
+                        child: SingleChildScrollView(
+                          padding: const EdgeInsets.all(64.0),
+                          child: Column(
+                            children: [
+                              const Text(
+                                'Something went wrong',
+                                style: TextStyle(
+                                  fontSize: 22.0,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }
+
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(
+                        child: SingleChildScrollView(
+                          padding: const EdgeInsets.all(64.0),
+                          child: Column(
+                            children: [
+                              const Text(
+                                'Loading',
+                                style: TextStyle(
+                                  fontSize: 22.0,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }
+
+                    return ListView(
+                      children: snapshot.data!.docs
+                          .map((DocumentSnapshot document) {
+                            Map<String, dynamic> data =
+                                document.data()! as Map<String, dynamic>;
+                            ChatEntry message = ChatEntry.fromMap(data);
+
+                            // Null/emptiness check
+                            if (message.message == null ||
+                                message.userEmail == null ||
+                                message.createdAt == null ||
+                                message.message == '' ||
+                                message.userEmail == '') {
+                              return Container();
+                            }
+
+                            String username = _getUsernameFromEmail(
+                              message.userEmail!,
+                            );
+
+                            return ListTile(
+                              title: Text(message.message!),
+                              subtitle: Text(
+                                'Sent by $username at ${message.createdAt!.toString()}',
+                              ),
+                            );
+                          })
+                          .toList()
+                          .cast(),
+                    );
+                  },
+            ),
+
+            SizedBox(height: 20.0),
+
+            // Chat message form
+            Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                spacing: 24.0,
+                children: [
+                  // Username field
+                  TextFormField(
+                    enabled: (_sendingMessage == 'ready'),
+                    controller: _messageController,
+                    decoration: InputDecoration(
+                      labelText: 'Chat Message',
+                      prefixIcon: const Icon(Icons.message),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+
+                  // Submit button
+                  ElevatedButton(
+                    onPressed: (_sendingMessage != 'ready')
+                        ? null
+                        : _submitChatForm,
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [Text('Send Message')],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
