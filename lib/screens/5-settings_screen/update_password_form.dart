64f2024fdd2355930e0c8e03109cb9aa3b5af355
Nathan Larsen <nlarsen@nathan2055.com>
2025-11-18 20:32:28 -0500

    Simplify UpdatePasswordForm implementation

--- lib/screens/5-settings_screen/update_password_form.dart
+++ lib/screens/5-settings_screen/update_password_form.dart
@@ -19,12 +19,10 @@ class _UpdatePasswordFormState extends State<UpdatePasswordForm> {
   final _formKey = GlobalKey<FormState>();
 
   // Text field controllers
-  final TextEditingController _oldPasswordController = TextEditingController();
-  final TextEditingController _newPasswordController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   // Track password visibility for the "show password" toggle
-  bool _isOldPasswordVisible = false;
-  bool _isNewPasswordVisible = false;
+  bool _isPasswordVisible = false;
 
   // A copy of the current user's info, populated upon initialization
   // This should only be null while the widget is loading
@@ -111,50 +109,25 @@ class _UpdatePasswordFormState extends State<UpdatePasswordForm> {
           mainAxisSize: MainAxisSize.min,
           spacing: 24.0,
           children: [
-            // Old Password field
+            // Password field
             TextFormField(
-              controller: _oldPasswordController,
-              obscureText: !_isOldPasswordVisible,
+              controller: _passwordController,
+              obscureText: !_isPasswordVisible,
               decoration: InputDecoration(
-                labelText: 'Old Password',
+                labelText: 'Password',
                 prefixIcon: const Icon(Icons.lock),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(12),
                 ),
                 suffixIcon: IconButton(
                   icon: Icon(
-                    _isOldPasswordVisible
+                    _isPasswordVisible
                         ? Icons.visibility
                         : Icons.visibility_off,
                   ),
                   onPressed: () {
                     setState(() {
-                      _isOldPasswordVisible = !_isOldPasswordVisible;
-                    });
-                  },
-                ),
-              ),
-            ),
-
-            // New Password field
-            TextFormField(
-              controller: _newPasswordController,
-              obscureText: !_isNewPasswordVisible,
-              decoration: InputDecoration(
-                labelText: 'New Password',
-                prefixIcon: const Icon(Icons.lock),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _isNewPasswordVisible
-                        ? Icons.visibility
-                        : Icons.visibility_off,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _isNewPasswordVisible = !_isNewPasswordVisible;
+                      _isPasswordVisible = !_isPasswordVisible;
                     });
                   },
                 ),
